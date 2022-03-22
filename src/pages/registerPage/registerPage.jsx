23fde55aaabd4f0c283fae2b05f63c74c1127111
sangzun-han <hsjun1996@naver.com>
2022-03-22 22:05:08 +0900

    feat : register

--- src/pages/registerPage/registerPage.jsx
+++ src/pages/registerPage/registerPage.jsx
@@ -1,11 +1,42 @@
-import React from "react";
+import clayful from "clayful/client-js";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./registerPage.module.css";
 
 const RegisterPage = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event) => {
+    setId(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const customer = clayful.Customer;
+
+    const payload = {
+      userId: id,
+      password: password,
+    };
+
+    customer.createMe(payload, (err, result) => {
+      if ((err.code = "duplicated-user-id"))
+        alert("이미 가입된 아이디 입니다.");
+      else {
+        console.log("result data : ", result.data);
+      }
+    });
+  };
+
   return (
     <div className={styles.auth_wrapper}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={styles.logo}>
           <img src="/images/logo.png" alt="logo" />
         </div>
@@ -13,22 +44,24 @@ const RegisterPage = () => {
           <input
             type="text"
             placeholder="아이디"
-            name="email"
-            defaultValue=""
+            name="id"
+            defaultValue={id}
+            onChange={handleEmailChange}
           />
           <input
             type="password"
             placeholder="비밀번호"
             name="password"
-            defaultValue=""
+            defaultValue={password}
+            onChange={handlePasswordChange}
           />
-          <input
+          {/* <input
             type="password"
             placeholder="비밀번호 확인"
             name="password"
             defaultValue=""
           />
-          <input type="email" placeholder="이름" name="email" defaultValue="" />
+          <input type="email" placeholder="이름" name="email" defaultValue="" /> */}
 
           <button type="submit">회원가입</button>
           <Link to="/login">이미 Apple ID가 있다면?</Link>
