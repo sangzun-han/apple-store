7d09a51bb0c125623cb2ba324f612be993cd1474
sangzun-han <hsjun1996@naver.com>
2022-03-22 12:11:36 +0900

    dependency clayful

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./app";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
   <React.StrictMode>
